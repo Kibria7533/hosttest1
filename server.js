70192671760c6d6a9605b6b6371e899e8e8c1a78
kibria7533 <tenminuteversity@gmail.com>
2020-12-18 18:57:47 +0600

    url updedet

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ if (process.env.NODE_ENV === "production") {
   // Set the static assets folder (ie, client build)
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
